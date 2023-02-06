0e72b415c91641bc8e1fabe8d7a280d42d00db1e
BloodSample <>
2023-02-06 22:20:17 +0400

    movement and shooting in progress

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    Rigidbody2D rb;
+    [SerializeField] float speed;
+    Vector2 velocity;
+    private void Awake()
+    {
+        rb = GetComponentInChildren<Rigidbody2D>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Shoot();
+    }
+    void Move()
+    {
+        velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = velocity * speed;
+    }
+    void Shoot()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject PlayerBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+        }
+    }    
+}
